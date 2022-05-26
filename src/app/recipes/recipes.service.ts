a165a1a0f76d9373f07e81be281a30cb554d372d
Kgaogel09 <kgaogelo@swipeix.com>
2022-05-26 11:34:23 +0200

    ionic components

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -11,15 +11,22 @@ export class RecipesService {
       id: 'r1',
       title: 'Schnitzel',
       imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/220px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+        'https://images.unsplash.com/photo-1599921841143-819065a55cc6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031',
       ingredients: ['French Fries', 'Pork Meat', 'Salad'],
     },
     {
       id: 'r2',
       title: 'Spaghetti',
       imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg/250px-Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg',
+        'https://images.unsplash.com/photo-1612524681749-2f7615663729?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3BhZ2hldHRpJTIwYm9sb2duZXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500',
       ingredients: ['Spaghetti', 'Ground Beef', 'Tomato'],
+    },
+    {
+      id: 'r3',
+      title: 'Chicken wings',
+      imageUrl:
+        'https://images.unsplash.com/photo-1624153064067-566cae78993d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hpY2tlbiUyMHdpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500',
+      ingredients: ['Chicken', 'Flower', 'Sauce'],
     },
   ];
 
