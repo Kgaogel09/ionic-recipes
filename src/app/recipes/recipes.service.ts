8db02ea93f7ece3faf5613f2f32e8c4da48a61c4
Kgaogel09 <kgaogelo@swipeix.com>
2022-05-26 09:05:46 +0200

    created services

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipesService {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/220px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad'],
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg/250px-Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg',
+      ingredients: ['Spaghetti', 'Ground Beef', 'Tomato'],
+    },
+  ];
+
+  constructor() {}
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find((recipe) => recipe.id === recipeId),
+    };
+  }
+}
